e95e22a9bf4ced1bb332a313b9ad4e9f6770bc7d
huang <761231770@qq.com>
2021-06-25 11:54:15 +0800

    新增测试单元

---
+++ src/test/java/hello/admincontrol/service/impl/MeetingServiceImplTest.java
@@ -0,0 +1,12 @@
+package hello.admincontrol.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MeetingServiceImplTest {
+
+    @Test
+    void latestCalenderThirtyDay() {
+    }
+}
